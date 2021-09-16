cc3ec022f70134da00bc5266038c30e36353a0d0
ninoish <ninoish.dev@gmail.com>
2021-09-16 23:18:29 +0900

    Update testTemplateCreator

--- force-app/main/default/lwc/testDataCreator/testDataCreator.js
+++ force-app/main/default/lwc/testDataCreator/testDataCreator.js
@@ -3,15 +3,14 @@ import { showToast } from "c/webFormUtils";
 import createTestTemplate from "@salesforce/apex/DAF_TestTemplateCreator.createTestTemplate";
 
 export default class TestDataCreator extends LightningElement {
-  handleClickCreateTestData() {
-    createTestTemplate()
-      .then((ret) => {
-        showToast(this, "成功", ret, "success");
-        console.log(ret);
-      })
-      .catch((err) => {
-        showToast(this, "エラー", err, "error");
-        console.error(err);
-      });
+  async handleClickCreateTestData() {
+    try {
+      const message = await createTestTemplate();
+      showToast(this, "成功", message, "success");
+      console.log(message);
+    } catch (err) {
+      showToast(this, "エラー", err.body.message, "error");
+      console.error(err);
+    }
   }
 }
